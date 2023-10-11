a03da8a7814ce878d8169babca7d7abd78510a70
Bela016 <126262449+Bela016@users.noreply.github.com>
2023-10-11 15:50:09 -0300

    Initial commit

---
+++ src/model/bebe.ts
@@ -0,0 +1,14 @@
+export abstract class Bebe{
+    private nome : string;
+    private peso :number;
+    private altura : number;
+
+    public constructor(_nome:string,_peso:number,_altura:number){
+        this.nome = _nome;
+        this.peso = _peso;
+        this.altura =_altura;
+    }
+    public setNome
+
+}
+
